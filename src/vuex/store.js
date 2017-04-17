f1a25c5313a8ed9640e9e7f07cc7677d97e432e9
Moudicat <i@0u0b.com>
2017-04-17 18:02:59 +0800

    Simple counter.

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,3 +1,20 @@
 /**
  * Created by reddy on 2017/4/17.
  */
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 0
+  },
+  // 通过mutations改变state的值
+  mutations: {
+    increment (state) {
+      state.count++;
+    }
+  }
+})
